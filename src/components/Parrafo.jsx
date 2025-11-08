dba3c8a51c391fe062cbe0536ec5343b25300bbb
OscarTorresQuintero <oscartquintero23@gmail.com>
2025-11-08 11:40:02 -0500

    Delete src/components/Parrafo.jsx

--- src/components/Parrafo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Parrafo() {
-  return (
-    <p className="text-gray-700 text-lg">
-      ¡Bienvenido a EcoHuerta! Cultiva tu propio jardín ecológico con nosotros.
-    </p>
-  );
-}
